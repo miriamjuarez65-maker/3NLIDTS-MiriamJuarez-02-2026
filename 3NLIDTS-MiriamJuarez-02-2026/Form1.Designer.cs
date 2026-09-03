ac5942b1ab1c6b906f281f7627b2ab560dbd5f45
miri2 <miri2@MIRIAM>
2026-09-02 18:16:38 -0600

    Agregar archivos de proyecto.

---
+++ 3NLIDTS-MiriamJuarez-02-2026/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace _3NLIDTS_MiriamJuarez_02_2026
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbCelcius = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbFahrenheit = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbKelvin = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Temp. en Celsius:";
+            // 
+            // tbCelcius
+            // 
+            this.tbCelcius.Location = new System.Drawing.Point(77, 78);
+            this.tbCelcius.Name = "tbCelcius";
+            this.tbCelcius.Size = new System.Drawing.Size(100, 20);
+            this.tbCelcius.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(46, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "tenp. en Fahrenheit:";
+            // 
+            // tbFahrenheit
+            // 
+            this.tbFahrenheit.Location = new System.Drawing.Point(77, 133);
+            this.tbFahrenheit.Name = "tbFahrenheit";
+            this.tbFahrenheit.Size = new System.Drawing.Size(100, 20);
+            this.tbFahrenheit.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(61, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Temp. en Kelvin:";
+            // 
+            // tbKelvin
+            // 
+            this.tbKelvin.Location = new System.Drawing.Point(77, 207);
+            this.tbKelvin.Name = "tbKelvin";
+            this.tbKelvin.Size = new System.Drawing.Size(100, 20);
+            this.tbKelvin.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnCalcular.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(25, 247);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(90, 33);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnBorrar.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.Location = new System.Drawing.Point(126, 247);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(93, 33);
+            this.btnBorrar.TabIndex = 7;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::_3NLIDTS_MiriamJuarez_02_2026.Properties.Resources.thermometer_on_the_mountains_in_the_snow_shows_temperatures_below_zero_low_temperatures_in_degrees_celsius_and_fahrenheit_photo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(384, 511);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.tbKelvin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbFahrenheit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbCelcius);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbCelcius;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbFahrenheit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbKelvin;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnBorrar;
+    }
+}
+
---
+++ 3NLIDTS-MiriamJuarez-02-2026/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3NLIDTS_MiriamJuarez_02_2026
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (tbCelcius.Text != "" || tbFahrenheit.Text != "" || tbKelvin.Text != "")
+            {
+                if (!string.IsNullOrEmpty(tbCelcius.Text))
+                {
+                    float Cc = float.Parse(tbCelcius.Text);
+
+                    float Fa2 = (Cc * 9f / 5f) + 32;
+                    tbFahrenheit.Text = Fa2.ToString();
+
+                    float Kc = Cc + 273;
+                    tbKelvin.Text = Kc.ToString();
+                }
+                else if (!string.IsNullOrEmpty(tbFahrenheit.Text))
+                {
+                    float Fa = float.Parse(tbFahrenheit.Text);
+
+                    float Cc = (Fa - 32) * 5.0f / 9.0f;
+                    tbCelcius.Text = Cc.ToString();
+
+                    float Kc = Cc + 273;
+                    tbKelvin.Text = Kc.ToString();
+                }
+                else if (!string.IsNullOrEmpty(tbKelvin.Text))
+                {
+                    float Kc = float.Parse(tbKelvin.Text);
+
+                    float Cc = Kc - 273;
+                    tbCelcius.Text = Cc.ToString();
+
+                    float Fa2 = (Cc * 9f / 5f) + 32;
+                    tbFahrenheit.Text = Fa2.ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese valores para el calculo de temperatura");
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Se Borraron los valores de los TextBox");
+
+            tbCelcius.Clear();
+            tbFahrenheit.Clear();
+            tbKelvin.Clear();
+        }
+    }
+}
